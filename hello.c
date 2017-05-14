eca6dc1e867f3e178d87bebdc7eb0705edc14c7b
Moshiur Rahman <unimrgm@gmail.com>
2017-05-14 16:43:42 +0600

    introduction to c programming

---
+++ hello.c
@@ -0,0 +1,12 @@
+ #include <stdio.h>
+
+ int main()
+ {
+    double radious,pi=3.1416,area;
+    printf("Enter the value of floor : \n");
+    scanf("%lf",&radious);
+    area= pi*radious*radious;
+    printf("The area is %.2lf",area);
+    return 0;
+
+ }
